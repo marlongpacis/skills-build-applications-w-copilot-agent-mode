a62eb757da3cab109361be7b6d0afc1239565fa0
marlongpacis <marlon.g.pacis@accenture.com>
2026-03-27 01:03:47 +0000

    Fix ALLOWED_HOSTS logic in settings.py to match Codespace and localhost requirements

--- octofit-tracker/backend/octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit-tracker/backend/octofit_tracker/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +27,10 @@ SECRET_KEY = 'django-insecure-&hdm5u81mq2k9td5rwh0*#*l7gt5n+u76vxuvh3-%@vt*=etp8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if os.environ.get('CODESPACE_NAME'):
+    ALLOWED_HOSTS.append(f"{os.environ.get('CODESPACE_NAME')}-8000.app.github.dev")
 
 
 # Application definition
@@ -145,8 +150,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-# Allow all hosts
-ALLOWED_HOSTS = ['*']
 
 # CORS settings
 CORS_ALLOW_ALL_ORIGINS = True
